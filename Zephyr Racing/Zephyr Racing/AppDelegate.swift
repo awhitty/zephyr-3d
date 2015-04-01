92e159f44b558bd7e0b2573bd2dbb9b124c1ac4d
Jason van der Merwe <jason.merwe@gmail.com>
2015-04-01 09:26:23 -0700

    newest material for 3ddrawing

--- Zephyr Racing/Zephyr Racing/AppDelegate.swift
+++ Zephyr Racing/Zephyr Racing/AppDelegate.swift
@@ -12,10 +12,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var mainViewController: UIViewController?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window!.backgroundColor = UIColor.whiteColor()
+        self.mainViewController = HomeController()
+        
+        self.window!.rootViewController = self.mainViewController
+        self.window!.makeKeyAndVisible()
         return true
     }
 
--- Zephyr Racing/Zephyr Racing/HomeController.swift
+++ Zephyr Racing/Zephyr Racing/HomeController.swift
@@ -1,39 +1,37 @@
 //
-//  ViewController.swift
+//  HomeController.swift
 //  Zephyr Racing
 //
-//  Created by Jason van der Merwe on 3/10/15.
+//  Created by Jason van der Merwe on 3/25/15.
 //  Copyright (c) 2015 Jason van der Merwe. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setupViews()
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        view.backgroundColor = UIColor.purpleColor();
+        view.backgroundColor = UIColor.orangeColor();
     }
-    
+   
     func setupViews(){
         var view1: UIView! = UIView()
         view1.frame = CGRectMake(0, 0, 100, 100)
-        view1.backgroundColor = UIColor.orangeColor()
+        view1.backgroundColor = UIColor.purpleColor()
         view.addSubview(view1)
     }
 
 }
-
